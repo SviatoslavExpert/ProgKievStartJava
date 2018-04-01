8374a83785991e37664b277452b6ce975322179f
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 19:21:41 +0300

    task

--- src/lesson01/LessonTaskOne.java
+++ src/lesson01/LessonTaskOne.java
@@ -1,4 +1,33 @@
+/*
+Написать программу которая считывает 5-и значное число с
+клавиатуры и выводит цифры из которого оно состоит.
+Например : Считывается число 54698
+Выводиться:
+5
+4
+6
+9
+8
+Деление по модулю %
+Результат деления по модулю - остаток от деления. Например, 7/4 равно 1 с остатком 3. Поэтому 7%4 = 3.
+ */
+
 package lesson01;
 
+import java.util.Scanner;
+
 public class LessonTaskOne {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите пятизначное число:");
+        int num = sc.nextInt();
+
+        System.out.println(num / 10000);
+        System.out.println(num % 10000 / 1000);
+        System.out.println(num % 1000 / 100);
+        System.out.println(num % 100 / 10);
+        System.out.println(num % 10);
+        sc.close();
+    }
 }
