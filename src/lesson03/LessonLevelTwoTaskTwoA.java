894f909d482f0025dac872365f31972d9ca49cc4
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 21:54:43 +0300

    task

---
+++ src/lesson03/LessonLevelTwoTaskTwoA.java
@@ -0,0 +1,27 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class LessonLevelTwoTaskTwoA {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        int floor = 0;
+        int access = 0;
+        System.out.println("Input numbers and press Enter");
+        number = sc.nextInt();
+
+        if (number > 0 && number <= 4 * 9 * 4) {
+            access = (number - 1) / 36 + 1;
+            number = number - 36 * (access - 1);
+            floor = (number + 3) / 4;
+            System.out.println("Access: " + access);
+            System.out.println("Floor: " + floor);
+        } else {
+            System.out.println("There is no such apartment");
+        }
+        sc.close();
+    }
+}
