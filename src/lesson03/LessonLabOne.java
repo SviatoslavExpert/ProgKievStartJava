0be6bfcf5b69a42ad340be28262b358506572796
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 21:25:34 +0300

    task

--- src/lesson03/LessonLabOne.java
+++ src/lesson03/LessonLabOne.java
@@ -1,4 +1,27 @@
 package lesson03;
 
+import java.util.Scanner;
+
 public class LessonLabOne {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input price one:");
+        int priceOne = sc.nextInt();
+
+        System.out.println("Input price two:");
+        int priceTwo = sc.nextInt();
+
+        System.out.println("Input price three:");
+        int priceThree = sc.nextInt();
+
+        int maxPrice = priceOne;
+        if(priceTwo > maxPrice) {
+            maxPrice = priceTwo;
+        }
+        if(priceThree > maxPrice) {
+            maxPrice = priceThree;
+        }
+        System.out.println("Max price: " + maxPrice + " UAH");
+    }
 }
