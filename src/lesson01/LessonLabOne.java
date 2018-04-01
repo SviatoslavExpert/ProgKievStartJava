7d4cf17389889ebb8b609646422f6acbd15c5b14
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 18:58:47 +0300

    task

--- src/lesson01/LessonLabOne.java
+++ src/lesson01/LessonLabOne.java
@@ -1,4 +1,13 @@
 package lesson01;
 
 public class LessonLabOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(5 + 15);
+    }
 }
+
+/*
+    System.out.println();     (Sysout = cntrl + tab)
+    don't make comments unless they are needed
+ */
