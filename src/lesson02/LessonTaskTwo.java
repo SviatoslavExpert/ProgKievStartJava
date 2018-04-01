8bf93c266f78727423e3a5f56012e4d1b18ee8c5
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 19:52:34 +0300

    task

--- src/lesson02/LessonTaskTwo.java
+++ src/lesson02/LessonTaskTwo.java
@@ -6,5 +6,15 @@
 package lesson02;
 
 public class LessonTaskTwo {
+    public static void main(String[] args) {
 
+        double a = 0.3;
+        double b = 0.5;
+        double c = 0.4;
+
+        double p = (a + b + c) / 2.0;   /*  очень важно: деление на вещественное число   */
+
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        System.out.println("Area = " + area);
+    }
 }
