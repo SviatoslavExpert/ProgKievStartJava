7106dc82b90cfb8293b994ef0211ed92bf45faab
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 19:36:53 +0300

    task

--- src/lesson02/LessonLabOne.java
+++ src/lesson02/LessonLabOne.java
@@ -1,4 +1,20 @@
 package lesson02;
 
+import java.util.Scanner;
+
 public class LessonLabOne {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        double price = 28.89;
+        double fuel = 8.5;
+        System.out.println("Input r (km) and press Enter");
+        int r = sc.nextInt();
+        double total;
+
+        total = fuel / 100.0 * price * r * 2.0;
+
+        System.out.println(total + " UAH");
+        sc.close();
+    }
 }
