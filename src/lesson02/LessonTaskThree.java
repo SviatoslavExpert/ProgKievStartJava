1ec07adc23d1b3a5589900cf17ad8121e68193e4
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 19:59:35 +0300

    task

--- src/lesson02/LessonTaskThree.java
+++ src/lesson02/LessonTaskThree.java
@@ -1,4 +1,22 @@
+/*
+Написать программу которая вычислит и выведет на экран
+площадь треугольника если известны его стороны.
+ */
+
 package lesson02;
 
+import java.util.Scanner;
+
 public class LessonTaskThree {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input r");
+        double r = sc.nextDouble();
+
+        double p = Math.PI * 2.0 * r;
+        System.out.println("Perimeter = " + p);
+        sc.close();
+    }
 }
