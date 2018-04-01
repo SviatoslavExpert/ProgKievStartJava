7074f5c6376b522f439f3cda2519b788e921db11
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-01 22:18:46 +0300

    task

--- src/lesson03/LessonLevelOneTaskThreeA.java
+++ src/lesson03/LessonLevelOneTaskThreeA.java
@@ -17,12 +17,12 @@ public class LessonLevelOneTaskThreeA {
         // TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
         int year;
-        System.out.println("Input year");
+        System.out.println("Input a year");
         year = sc.nextInt();
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            System.out.println(year + " is leap year");
+            System.out.println(year + " is a leap year.");
         } else {
-            System.out.println(year + " is not leap year");
+            System.out.println(year + " is not a leap year.");
         }
         sc.close();
     }
--- src/lesson03/LessonLevelOneTaskThreeB.java
+++ src/lesson03/LessonLevelOneTaskThreeB.java
@@ -1,4 +1,28 @@
+/*
+Определить количество дней в году, который вводит пользователь. В
+високосном годе - 366 дней, тогда как в обычном 365. Високосный год
+определяется по следующему правилу:
+Год високосный, если он делится на четыре без остатка, но если он
+делится на 100 без остатка, это не високосный год. Однако, если он
+делится без остатка на 400, это високосный год.
+ */
+
 package lesson03;
 
+import java.util.Scanner;
+
 public class LessonLevelOneTaskThreeB {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year:");
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is a leap year.");
+        }
+    }
 }
